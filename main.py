7a3f3f0e35d74c7c4647fe7d7e55583ba2a77ad4
bhosaleparas <124509431+bhosaleparas@users.noreply.github.com>
2025-12-07 14:08:59 +0530

    Add files via upload
    added delete update filter methods for employee

--- main.py
+++ main.py
@@ -78,3 +78,78 @@ def get_employee(employee_id:int ,db:Session=Depends(get_db)):
         )
     
     return employee
+
+
+@app.put('/employees/{emp_id}',response_model=schemas.EmployeeResponse)
+def update_employee(emp_id:int,employee_update:schemas.EmployeeUpdate,db:Session=Depends(get_db)):
+    db_employee=db.query(models.Employee).filter(models.Employee.id==emp_id).first()
+    
+    if not db_employee:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"employee not found for is {emp_id}"
+        )
+        
+    # if email is updated check if new emil exist or not
+    
+    if employee_update.email and employee_update.email !=db_employee:
+        exiisting_employee=db.query(models.Employee).filter(models.Employee.email==employee_update.email).first()
+        
+        if exiisting_employee:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already exists"
+            )
+        
+        # update employee
+        update_data=employee_update.model_dump(exclude_unset=True)
+        for field,value in update_data.items():
+            if value is not None:
+                setattr(db_employee,field,value)
+                
+        db.commit()
+        db.refresh(db_employee)
+        return db_employee
+    
+    
+    
+# delete employee
+@app.delete('/employees/{emp_id}')
+def delete_employee(emp_id:int,db:Session=Depends(get_db)):
+    employee=db.query(models.Employee).filter(models.Employee.id==emp_id).first()
+    
+    if not employee:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"employee not found for is {emp_id}"
+        )
+        
+        
+    db.delete(employee)
+    db.commit()
+    
+    return {"message":f"employee with id {emp_id} deleted successfully"}
+
+
+
+
+# Search employees by department
+@app.get("/employees/search/department/{department}")
+def search_by_department(department: str, db: Session = Depends(get_db)):
+    employees = db.query(models.Employee).filter(
+        models.Employee.department.ilike(f"%{department}%")
+    ).all()
+    
+    return employees
+
+
+
+
+# Search employees by name
+@app.get("/employees/search/name/{name}")
+def search_by_name(name: str, db: Session = Depends(get_db)):
+    employees = db.query(models.Employee).filter(
+        models.Employee.name.ilike(f"%{name}%")
+    ).all()
+    
+    return employees
